5a2338d5c628e5c15dd397c3d6e271cd340844b9
Asif <asif.aka01@gmail.com>
2024-03-03 01:41:41 +0530

    Optimized the code

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 import RoomCard from "../components/RoomCard";
-import { useSelector, useDispatch, RootStateOrAny } from 'react-redux';
+import { useSelector, useDispatch, RootStateOrAny } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { IRoom } from '../interfaces/IRoom';
+import { IRoom } from "../interfaces/IRoom";
 import SearchRooms from "../components/SearchRooms";
-import { fetchRooms } from '../redux/actions/RoomActions';
+import { fetchRooms } from "../redux/actions/RoomActions";
 import Paginate from "../components/Paginate";
 
-
 const HomeScreen = () => {
-
   const dispatch = useDispatch();
 
   const [keyword, setKeyword] = useState<string>("");
@@ -19,7 +17,9 @@ const HomeScreen = () => {
   const [roomType, setRoomType] = useState<string>("");
 
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const { loading, rooms, count, error } = useSelector((state: RootStateOrAny) => state.roomsFetch);
+  const { loading, rooms, count, error } = useSelector(
+    (state: RootStateOrAny) => state.roomsFetch
+  );
 
   useEffect(() => {
     dispatch(fetchRooms(keyword, numOfBeds, roomType, currentPage));
@@ -32,8 +32,8 @@ const HomeScreen = () => {
           <h2 className="mb-4">All Rooms</h2>
         </Col>
       </Row>
-      <SearchRooms 
-        keyword={keyword} 
+      <SearchRooms
+        keyword={keyword}
         setKeyword={setKeyword}
         numOfBeds={numOfBeds}
         setNumOfBeds={setNumOfBeds}
@@ -41,15 +41,19 @@ const HomeScreen = () => {
         setRoomType={setRoomType}
       />
       <Row>
-        {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : rooms.length > 0 ?
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : rooms && rooms.length > 0 ? (
           <>
-            {rooms.map((room: IRoom) =>
-              <Col key={room._id} md={3} sm={6} xs={12} >
+            {rooms.map((room: IRoom) => (
+              <Col key={room._id} md={3} sm={6} xs={12}>
                 <RoomCard {...room} />
               </Col>
-            )}
+            ))}
           </>
-        : (
+        ) : (
           <>
             <Message variant="info">No Room Available</Message>
           </>
